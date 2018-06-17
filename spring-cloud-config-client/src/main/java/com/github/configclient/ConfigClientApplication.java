c82f376a7db8b7bf36bbf0ef88cb0d339659cd0c
dujf <king54732@163.com>
2018-06-17 15:01:42 +0800

    服务配置中心，congfig server,和获取配置的客户端

---
+++ spring-cloud-config-client/src/main/java/com/github/configclient/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.github.configclient;
+
+import com.github.configclient.config.ServiceInfoUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+        String line = "============================================";
+        System.out.println(line + "\n http://localhost:" + ServiceInfoUtil.getPort() + " \n" + line);
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @GetMapping("/name")
+    public String getName() {
+        return name;
+    }
+}
