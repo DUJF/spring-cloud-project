1a36da112b22f9dccf07746b75e6dbf2acf434ab
dujf <king54732@163.com>
2018-06-18 10:17:53 +0800

    Spring Boot Admin管理spring cloud 服务

--- spring-boot-admin/src/main/java/com/github/admin/AdminApplication.java
+++ spring-boot-admin/src/main/java/com/github/admin/AdminApplication.java
@@ -1,12 +1,23 @@
 package com.github.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * 程序监控
+ **/
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+        String line = "============================================";
+        System.out.println(line + "\n http://localhost:8016 \n" + line);
+    }
 }
