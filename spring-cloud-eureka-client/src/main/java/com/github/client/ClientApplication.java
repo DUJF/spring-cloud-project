6fe1fb972fbdaac09eb7e15264c7e602d7457c4a
dujf <king54732@163.com>
2018-06-18 17:59:03 +0800

    spring zuul 使用swagger生成统一的微服务接口管理文档

--- spring-cloud-eureka-client/src/main/java/com/github/client/ClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/github/client/ClientApplication.java
@@ -1,10 +1,12 @@
 package com.github.client;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableSwagger2Doc
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- spring-cloud-zuul/src/main/java/com/github/zuul/ZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/github/zuul/ZuulApplication.java
@@ -1,15 +1,22 @@
 package com.github.zuul;
 
-import com.didispace.swagger.butler.EnableSwaggerButler;
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-//@EnableSwaggerButler
+@EnableSwagger2Doc
 public class ZuulApplication {
 
     public static void main(String[] args) {
@@ -17,5 +24,26 @@ public class ZuulApplication {
         String line = "============================================";
         System.out.println(line + "\n http://localhost:8013 \n" + line);
 
+    }
+
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+        @Override
+        public List<SwaggerResource> get() {
+            List resources = new ArrayList<>();
+            //location   name :api-a 是文档名称 /api-a是添加的路由配置
+            resources.add(swaggerResource("api-a", "/api-a/v2/api-docs", "1.0"));
+            resources.add(swaggerResource("api-b", "/api-b/v2/api-docs", "1.0"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
     }
 }
