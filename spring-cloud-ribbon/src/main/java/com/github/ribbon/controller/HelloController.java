cf76e5917db45639bc636dfd559570f6d1d97bd7
dujf <king54732@163.com>
2018-09-17 12:54:18 +0800

    feat : add redis auto incr id

--- spring-cloud-ribbon/src/main/java/com/github/ribbon/controller/HelloController.java
+++ spring-cloud-ribbon/src/main/java/com/github/ribbon/controller/HelloController.java
@@ -2,18 +2,26 @@ package com.github.ribbon.controller;
 
 import com.github.ribbon.service.HelloServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("hello")
 public class HelloController {
 
   @Autowired
   private HelloServiceImpl helloService;
 
+  @Autowired
+  private RedisTemplate redisTemplate;
 
-  @GetMapping
-  public String Hello() {
-    return helloService.getHello();
+//  @GetMapping
+//  public String Hello() {
+//    return helloService.getHello();
+//  }
+
+  @GetMapping("id")
+  public Long getId() {
+    return redisTemplate.opsForHash().increment("", "", 1L);
   }
 }
