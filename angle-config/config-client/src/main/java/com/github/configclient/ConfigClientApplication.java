faa730d1f123d9042c9ff3d020c1d4850d649124
dujf <king54732@163.com>
2018-11-15 14:19:28 +0800

    fix: 修改配置中心目录结构

--- angle-config/config-client/src/main/java/com/github/configclient/ConfigClientApplication.java
+++ angle-config/config-client/src/main/java/com/github/configclient/ConfigClientApplication.java
@@ -6,18 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author dujf
+ */
 @SpringBootApplication
 @RestController
 @RefreshScope
+@EnableSwagger2
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigClientApplication.class, args);
     String line = "============================================";
+    System.out.println(new StringBuffer(line).append("\n http://localhost:8015 \n").append(line));
   }
 
-//  @Value("${name}")
+  @Value("${name}")
   private String name;
 
   @GetMapping("/name")
